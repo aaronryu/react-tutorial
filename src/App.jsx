e3c12d274083d2e6543133be4190f5d3af5e7be4
Aaron Ryu <fbcndah@gmail.com>
2024-09-11 09:49:37 +0900

    실습 5-3: 다수의 State 를 하나의 객체 State 로 합치기

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,14 @@
 import { useState } from 'react'
 import '@/App.css'
 
-const FEE_ADULT = 20000
-const FEE_NON_ADULT = 10000
+const ADULT = { valid: true, entrance: 20000 }
+const NON_ADULT = { valid: false, entrance: 10000 }
 
 function App() {
   const [age, setAge] = useState(0)
-  const [valid, setValid] = useState(false)
-  const [entrance, setEntrance] = useState(FEE_NON_ADULT)
-
-  function setCalculate(input) {
-    if (input >= 19) {
-      setValid(true)
-      setEntrance(FEE_ADULT)
-    } else {
-      setValid(false)
-      setEntrance(FEE_NON_ADULT)
-    }
-  }
+  // const [valid, setValid] = useState(false)
+  // const [entrance, setEntrance] = useState(FEE_NON_ADULT)
+  const [calculate, setCalculate] = useState(NON_ADULT)
 
   return (
     <>
@@ -31,11 +22,11 @@ function App() {
           // 성년이 되어도 15000원이 되지 않습니다. 여러분들이 각자 한번 풀어보세요.
           // setEntrance(valid ? FEE_ADULT : FEE_NON_ADULT)
           setAge(changed)
-          setCalculate(changed)
+          setCalculate(changed >= 19 ? ADULT : NON_ADULT)
         }}
       />
-      {valid ? <div>성년입니다.</div> : <div style={{ color: 'red' }}>미성년입니다.</div>}
-      <div>{`${entrance}원`}</div>
+      {calculate.valid ? <div>성년입니다.</div> : <div style={{ color: 'red' }}>미성년입니다.</div>}
+      <div>{`${calculate.entrance}원`}</div>
     </>
   )
 }
