8d75a61611a918cc1255870057416b6415758a6d
Aaron Ryu <fbcndah@gmail.com>
2024-09-11 09:38:32 +0900

    실습 3: 삼항 연산자를 활용한 조건부 컴포넌트 노출

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,28 @@
 import { useState } from 'react'
-import reactLogo from '@/assets/react.svg'
-import viteLogo from '/vite.svg'
 import '@/App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [age, setAge] = useState(0)
+  const [valid, setValid] = useState(false /* 성년 여부 */)
+
+  function handleInput(event) {
+    const changed = event.currentTarget.value
+    setAge(changed)
+    setValid(changed >= 19)
+  }
 
   return (
     <>
-      <div>
-        <a href='https://vitejs.dev' target='_blank'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://react.dev' target='_blank'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className='card'>
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
+      <input type='number' value={age} onChange={handleInput} />
+      {/* 1. 단일 컴포넌트 내 표기할 값 변경 */}
+      {/* - valid === true 일때 뒤엣것을 표기하고, false 일때 boolean false 를 반환하나 JSX 에선 undefined, null, false, true 모두 표기하지 않음 */}
+      {undefined}
+      {null}
+      {false}
+      {true}
+      {/* <div>{valid && '성년입니다.'}</div> */}
+      {/* 2. 상태값에 따라 다른 컴포넌트 노출 (삼항연산자) */}
+      {valid ? <div>성년입니다.</div> : <div style={{ color: 'red' }}>미성년입니다.</div>}
     </>
   )
 }
