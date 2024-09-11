80a88dd30b59f19f24beae964a8b977edb6a5e0b
Aaron Ryu <fbcndah@gmail.com>
2024-09-12 06:06:32 +0900

    실습 11-3: useRef 통한 <video> 태그 DOM(HTML 요소) 영상 소스 변경

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,20 @@ import { useRef } from 'react'
 
 function App() {
   const reference = useRef(null)
+  const sources = [
+    'https://vjs.zencdn.net/v/oceans.mp4',
+    'https://lamberta.github.io/html5-animation/examples/ch04/assets/movieclip.mp4',
+  ]
 
   console.log('- rerendered')
 
   return (
     <>
-      <div ref={reference}>apple</div>
-      <button onClick={(e) => (reference.current.style.color = 'red')}>변경</button>
+      <video ref={reference} autoPlay controls width={500} />
+      <div>
+        <button onClick={() => (reference.current.src = sources[0])}>전환 1</button>
+        <button onClick={() => (reference.current.src = sources[1])}>전환 2</button>
+      </div>
     </>
   )
 }
