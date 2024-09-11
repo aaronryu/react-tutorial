8504c2fa99632520ac1ba0563f239f63460194b9
Aaron Ryu <fbcndah@gmail.com>
2024-09-12 02:27:08 +0900

    실습 6-8: <input> 의 입력 이벤트 - setDesc() + setItems()

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import '@/App.css'
 import { useState } from 'react'
 
-function ListItem({ name, age, desc }) {
+function ListItem({ name, age, desc, setDesc }) {
   const [activated, setActivate] = useState(false)
   return (
     <li style={{ textAlign: 'left' }} onClick={(e) => setActivate(!activated)}>
       {name} | {age} |{' '}
       {activated ? (
-        <input value={desc} onClick={(e) => e.stopPropagation()} />
+        <input
+          value={desc}
+          onClick={(e) => e.stopPropagation()}
+          onChange={(e) => setDesc(e.currentTarget.value)}
+        />
       ) : (
         <span>{desc}</span>
       )}
@@ -15,28 +19,38 @@ function ListItem({ name, age, desc }) {
   )
 }
 
-function UnorderedList({ items }) {
+function UnorderedList({ items, setItems }) {
   return (
     <ul>
       {items.map((item, index) => (
-        <ListItem key={index} name={item.name} age={item.age} desc={item.desc} />
+        <ListItem
+          key={index}
+          name={item.name}
+          age={item.age}
+          desc={item.desc}
+          setDesc={(input) => {
+            const updated = [...items]
+            updated[index].desc = input
+            setItems(updated)
+          }}
+        />
       ))}
     </ul>
   )
 }
 
 function App() {
-  const items = [
+  const [items, setItems] = useState([
     { name: 'Aaron', age: 10, desc: '안녕하세요' },
     { name: 'Baron', age: 30, desc: '반갑습니다' },
     { name: 'Caron', age: 22, desc: '처음뵙겠습니다' },
     { name: 'Daron', age: 17, desc: '보고싶었습니다' },
-  ]
+  ])
   return (
     <>
       <div>
         {/* ul = Unordered List */}
-        <UnorderedList items={items} />
+        <UnorderedList items={items} setItems={setItems} />
         {/* ol = Ordered List */}
         <ol>
           {/* li = List Item */}
