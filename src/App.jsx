54dc0efe9177dadb4aa030557f945ba860119dfe
Aaron Ryu <fbcndah@gmail.com>
2024-09-11 09:45:10 +0900

    실습 4-1: 자식 컴포넌트 2개 생성 - SetState 할용 방식 2가지

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,53 @@
 import { useState } from 'react'
-import reactLogo from '@/assets/react.svg'
-import viteLogo from '/vite.svg'
 import '@/App.css'
+
+// 1. SetState 파라미터가 새로운 상태"값"
+function IncreaseButtonComponent({ count, setCount } /* Object Deconstructure 객체 비구조화 */) {
+  // console.log('props : ', props)
+  return (
+    <button
+      className='increase-button'
+      onClick={() => {
+        // props.setCount(props.count + 1)
+        setCount(count + 1)
+      }}
+    >
+      증가
+    </button>
+  )
+}
+
+// 2. SetState 파라미터가 새로운 상태를 생성하는 "함수"
+function DecreaseButtonComponent({ setCount }) {
+  return (
+    <button
+      className='decrease-button'
+      onClick={() => {
+        setCount((previousCount) => previousCount - 1)
+      }}
+    >
+      감소
+    </button>
+  )
+}
 
 function App() {
   const [count, setCount] = useState(0)
 
+  /**
+   * JSX 는 객체이고, 그 객체 내부에 props 라는 객체 프로퍼티를 갖는다. 총 2개의 파라미터를 우린 넘겨주고 있다.
+   *  - className : 'example'
+   *  - children  : 'example-children'
+   */
+  const jsx = <div className='example'>example-children</div>
+
   return (
     <>
+      <div>{count}</div>
       <div>
-        <a href='https://vitejs.dev' target='_blank'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://react.dev' target='_blank'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
+        <IncreaseButtonComponent count={count} setCount={setCount} />
+        <DecreaseButtonComponent setCount={setCount} />
       </div>
-      <h1>Vite + React</h1>
-      <div className='card'>
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
     </>
   )
 }
