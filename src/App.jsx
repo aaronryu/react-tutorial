d54dc996807ac0af8bfe6b69b6b19fdd562e4701
Aaron Ryu <fbcndah@gmail.com>
2024-09-11 09:45:58 +0900

    실습 4-2: 자식 공통 컴포넌트 하나 재사용 및 State Lifting

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,17 @@
 import { useState } from 'react'
 import '@/App.css'
 
-// 1. SetState 파라미터가 새로운 상태"값"
-function IncreaseButtonComponent({ count, setCount } /* Object Deconstructure 객체 비구조화 */) {
-  // console.log('props : ', props)
+/**
+ * State Lifting : 부모 컴포넌트가 가지고 있는 상태 변경을 위한 Controller = SetState 를 자식 컴포넌트가 호출
+ *  = 상태 끌어올리기 = 부모의 SetState 호출을 자식에게 넘긴다(콜백)
+ */
+function ButtonComponent(/* props → object destructure */ { className, onClick, children }) {
+  // const className = props.className
+  // const onClick = props.onClick
+  // const children = props.children
   return (
-    <button
-      className='increase-button'
-      onClick={() => {
-        // props.setCount(props.count + 1)
-        setCount(count + 1)
-      }}
-    >
-      증가
-    </button>
-  )
-}
-
-// 2. SetState 파라미터가 새로운 상태를 생성하는 "함수"
-function DecreaseButtonComponent({ setCount }) {
-  return (
-    <button
-      className='decrease-button'
-      onClick={() => {
-        setCount((previousCount) => previousCount - 1)
-      }}
-    >
-      감소
+    <button className={className} onClick={onClick}>
+      {children}
     </button>
   )
 }
@@ -45,8 +30,29 @@ function App() {
     <>
       <div>{count}</div>
       <div>
-        <IncreaseButtonComponent count={count} setCount={setCount} />
-        <DecreaseButtonComponent setCount={setCount} />
+        <ButtonComponent
+          // 1. string 을 그냥 넘길수도 있고
+          className='increase-button'
+          onClick={() => setCount((previousCount) => previousCount + 1)}
+          // onClick={() => {
+          //   setCount((previousCount) => previousCount + 1)
+          // }}
+        >
+          {/* 두개 이상의 컴포넌트는 주입이 불가능하다 */}
+          {/* <div></div> */}
+          {/* <div></div> */}
+          증가
+        </ButtonComponent>
+        <ButtonComponent
+          // 2. 중괄호 JS 로직으로 string 을 넘길수도 있다.
+          className={'decrease-button'}
+          onClick={() => setCount((previousCount) => previousCount - 1)}
+          // onClick={() => {
+          //   setCount((previousCount) => previousCount - 1)
+          // }}
+        >
+          감소
+        </ButtonComponent>
       </div>
     </>
   )
