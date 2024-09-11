2aeb7144bd50c4d8d988181a3f0d15a5dedf1f38
Aaron Ryu <fbcndah@gmail.com>
2024-09-11 09:48:00 +0900

    실습 5-1: SetState 는 동기적이 아닌 비동기적으로 동작

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,29 @@
 import { useState } from 'react'
-import reactLogo from '@/assets/react.svg'
-import viteLogo from '/vite.svg'
 import '@/App.css'
 
+const FEE_ADULT = 20000
+const FEE_NON_ADULT = 10000
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [age, setAge] = useState(0)
+  const [valid, setValid] = useState(false)
+  const [entrance, setEntrance] = useState(FEE_NON_ADULT)
 
   return (
     <>
-      <div>
-        <a href='https://vitejs.dev' target='_blank'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://react.dev' target='_blank'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className='card'>
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
+      <input
+        type='number'
+        value={age}
+        onChange={(event) => {
+          const changed = Number(event.currentTarget.value)
+          setAge(changed)
+          setValid(changed >= 19)
+          // 성년이 되어도 15000원이 되지 않습니다. 여러분들이 각자 한번 풀어보세요.
+          setEntrance(valid ? FEE_ADULT : FEE_NON_ADULT)
+        }}
+      />
+      {valid ? <div>성년입니다.</div> : <div style={{ color: 'red' }}>미성년입니다.</div>}
+      <div>{`${entrance}원`}</div>
     </>
   )
 }
