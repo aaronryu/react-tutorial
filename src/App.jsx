272e873cdab2b607c8cdb752a25dda71f213081c
Aaron Ryu <fbcndah@gmail.com>
2024-09-12 03:37:46 +0900

    실습 8: useState 시 리렌더링 / useRef 시 리렌더되지 않음

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,22 @@
-import { useState } from 'react'
-import reactLogo from '@/assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useRef, useState } from 'react'
 import '@/App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
+  const countReference = useRef(0)
+  // Rerender 가 일어날때마다 표기 | countReference 를 '조작'하고, '표기'했을때 어떠한 Rerender 도 발생하지 않음
+  console.log('- rerendered')
+
   return (
     <>
-      <div>
-        <a href='https://vitejs.dev' target='_blank'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://react.dev' target='_blank'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className='card'>
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
+      {/* 1. 표기 View */}
+      <div>State 값 : {count}</div>
+      <div>Reference 값 : {countReference.current}</div>
+      {/* 2. 조작 Controller */}
+      <button onClick={() => setCount(count + 1)}>State 값 증가</button>
+      <button onClick={() => (countReference.current += 1)}>Reference 값 증가</button>
+      <button onClick={() => console.log('지금 값은 : ', countReference.current)}>표기</button>
     </>
   )
 }
