5d84e2091c55a4be2849ccbc7fb5ac8c82f8e87e
Aaron Ryu <fbcndah@gmail.com>
2024-09-14 23:09:30 +0900

    실습 12-1: 아이디/패스워드 입력 컴포넌트 생성

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,31 @@
-import { useState } from 'react'
-import reactLogo from '@/assets/react.svg'
-import viteLogo from '/vite.svg'
 import '@/App.css'
 
+function UsernameInput() {
+  return (
+    <div>
+      Username : <input />
+    </div>
+  )
+}
+
+function PasswordInput() {
+  return (
+    <div>
+      Password : <input type='password' />
+      <button>🔓 보이기</button>
+    </div>
+  )
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+  function registration() {}
 
   return (
-    <>
-      <div>
-        <a href='https://vitejs.dev' target='_blank'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://react.dev' target='_blank'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className='card'>
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
-    </>
+    <section style={{ textAlign: 'start', width: 400 }}>
+      <UsernameInput />
+      <PasswordInput />
+      <button onClick={registration}>회원가입 완료</button>
+    </section>
   )
 }
 
