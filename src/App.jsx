ca413fbb4bc45e5a76a400dc50fa3592ab411770
Aaron Ryu <fbcndah@gmail.com>
2024-09-14 23:41:05 +0900

    실습 12-7: 유효성 검증 함수 및 상태 리프팅 (State Lifting)

--- src/App.jsx
+++ src/App.jsx
@@ -10,13 +10,7 @@ function UsernameInput({ reference }) {
   )
 }
 
-function PasswordInput({ reference }) {
-  const [valid, setValid] = useState({
-    maximum: false,
-    minimum: false,
-    required: false,
-  })
-
+function PasswordInput({ reference, valid, validate }) {
   function changeMode(e) {
     if (reference.current.type === 'password') {
       reference.current.type = 'text'
@@ -29,20 +23,7 @@ function PasswordInput({ reference }) {
   console.log('- 매번 입력해도 rerender 미발생. 객체 프로퍼티 단위 불변성을 유지하며 리렌더 방지')
   return (
     <div>
-      Password :{' '}
-      <input
-        type='password'
-        ref={reference}
-        onChange={(e) => {
-          const input = e.currentTarget.value
-          const changed = produce(valid, (draft) => {
-            if (valid.maximum !== input.length <= 10) draft.maximum = input.length <= 10
-            if (valid.minimum !== input.length > 5) draft.minimum = input.length > 5
-            if (valid.required !== input.length > 0) draft.required = input.length > 0
-          })
-          setValid(changed)
-        }}
-      />
+      Password : <input type='password' ref={reference} onChange={validate} />
       <button onClick={changeMode}>🔓 보이기</button>
       {valid.maximum || <div style={{ color: 'red' }}>비밀번호는 10글자를 넘을 수 없습니다.</div>}
       {valid.minimum || <div style={{ color: 'red' }}>비밀번호는 5글자를 넘어야합니다.</div>}
@@ -54,13 +35,28 @@ function PasswordInput({ reference }) {
 function App() {
   const usernameRef = useRef(null)
   const passwordRef = useRef(null)
+  const [passwordValid, setPasswordValid] = useState({
+    maximum: false,
+    minimum: false,
+    required: false,
+  })
+
+  function passwordValidate(e) {
+    const input = e.currentTarget.value
+    const changed = produce(passwordValid, (draft) => {
+      if (passwordValid.maximum !== input.length <= 10) draft.maximum = input.length <= 10
+      if (passwordValid.minimum !== input.length > 5) draft.minimum = input.length > 5
+      if (passwordValid.required !== input.length > 0) draft.required = input.length > 0
+    })
+    setPasswordValid(changed)
+  }
 
   function registration() {}
 
   return (
     <section style={{ textAlign: 'start', width: 400 }}>
       <UsernameInput reference={usernameRef} />
-      <PasswordInput reference={passwordRef} />
+      <PasswordInput reference={passwordRef} valid={passwordValid} validate={passwordValidate} />
       <button onClick={registration}>회원가입 완료</button>
     </section>
   )
