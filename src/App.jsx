712832e1c0f17212695bd0611fba9cf495bbfcd4
Aaron Ryu <fbcndah@gmail.com>
2024-09-15 00:22:14 +0900

    실습 12-9: React Hook Form 의 useForm 처럼 Custom Hook 자체개발

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function UsernameInput({ reference }) {
   )
 }
 
-function PasswordInput({ reference, valid, validate }) {
+function PasswordInput({ reference, error, validate }) {
   function changeMode(e) {
     if (reference.current.type === 'password') {
       reference.current.type = 'text'
@@ -26,30 +26,42 @@ function PasswordInput({ reference, valid, validate }) {
       Password :{' '}
       <input type='password' ref={reference} onChange={(e) => validate(e.target.value)} />
       <button onClick={changeMode}>🔓 보이기</button>
-      {valid.maximum || <div style={{ color: 'red' }}>비밀번호는 10글자를 넘을 수 없습니다.</div>}
-      {valid.minimum || <div style={{ color: 'red' }}>비밀번호는 5글자를 넘어야합니다.</div>}
-      {valid.required || <div style={{ color: 'red' }}>비밀번호를 입력해주세요.</div>}
+      <div style={{ color: 'red' }}>{error.maximum}</div>
+      <div style={{ color: 'red' }}>{error.minimum}</div>
+      <div style={{ color: 'red' }}>{error.required}</div>
     </div>
   )
 }
 
-function App() {
-  const usernameRef = useRef(null)
-  const passwordRef = useRef(null)
-  const [passwordValid, setPasswordValid] = useState({
+function useSingularForm() {
+  const ref = useRef(null)
+  const [valid, setValid] = useState({
     maximum: false,
     minimum: false,
     required: false,
   })
 
-  function passwordValidate(input) {
-    const changed = produce(passwordValid, (draft) => {
-      if (passwordValid.maximum !== input.length <= 10) draft.maximum = input.length <= 10
-      if (passwordValid.minimum !== input.length > 5) draft.minimum = input.length > 5
-      if (passwordValid.required !== input.length > 0) draft.required = input.length > 0
+  function validate(input) {
+    const changed = produce(valid, (draft) => {
+      if (valid.maximum !== input.length <= 10) draft.maximum = input.length <= 10
+      if (valid.minimum !== input.length > 5) draft.minimum = input.length > 5
+      if (valid.required !== input.length > 0) draft.required = input.length > 0
     })
-    setPasswordValid(changed)
+    if (!changed.maximum || !changed.minimum || !changed.required) ref.current?.focus()
+    setValid(changed)
   }
+
+  const error = {}
+  if (valid.maximum === false) error.maximum = '비밀번호는 10글자를 넘을 수 없습니다.'
+  if (valid.minimum === false) error.minimum = '비밀번호는 5글자를 넘어야합니다.'
+  if (valid.required === false) error.required = '비밀번호를 입력해주세요.'
+
+  return { ref, validate, error }
+}
+
+function App() {
+  const usernameRef = useRef(null)
+  const { ref: passwordRef, validate: passwordValidate, error: passwordError } = useSingularForm()
 
   function registration() {
     const request = {
@@ -63,7 +75,7 @@ function App() {
   return (
     <section style={{ textAlign: 'start', width: 400 }}>
       <UsernameInput reference={usernameRef} />
-      <PasswordInput reference={passwordRef} valid={passwordValid} validate={passwordValidate} />
+      <PasswordInput reference={passwordRef} error={passwordError} validate={passwordValidate} />
       <button onClick={registration}>회원가입 완료</button>
     </section>
   )
