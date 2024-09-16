21aad60b135d2c06b8af1051b4a470f63b111b91
Aaron Ryu <fbcndah@gmail.com>
2024-09-16 14:55:24 +0900

    실습 14: log 로 호출순서 파악 = 1. Render → 2. useLayoutEffect → (Paint) → 3. useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,29 @@
-import { useState } from 'react'
-import reactLogo from '@/assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useLayoutEffect, useState } from 'react'
 import '@/App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [value, setValue] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href='https://vitejs.dev' target='_blank'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://react.dev' target='_blank'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className='card'>
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
-    </>
-  )
+  // 각 Hook 내 조건부 setValue 코드 부분을 주석처리하면서 버튼 클릭 시 깜빡임의 유무를 눈으로 확인해보세요.
+  // 1. useEffect
+  useEffect(() => {
+    if (value === 0) {
+      setValue(10 + Math.random() * 200)
+    }
+    console.log('- 3. Paint 직후')
+  }, [value])
+
+  // 2. useLayoutEffect
+  useLayoutEffect(() => {
+    if (value === 0) {
+      setValue(10 + Math.random() * 200)
+    }
+    console.log('- 2. Paint 직전 = React 과정 중 (2) Commit 직후')
+  }, [value])
+
+  console.log('- 1. React 과정 중 (1) Render 과정')
+
+  return <button onClick={() => setValue(0)}>value: {value}</button>
 }
 
 export default App
