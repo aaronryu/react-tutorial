0495913c83e6f5b630a68d1a1d50e448ddf2289d
Aaron Ryu <fbcndah@gmail.com>
2024-09-16 15:13:02 +0900

    실습 15: 컴포넌트 <Input> 과 외부 <input> 요소 사이 State Lifting 시 useEffect 오용사례

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,37 @@
-import { useState } from 'react'
-import reactLogo from '@/assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import '@/App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+function Input({ value, onChange }) {
+  const [innerValue, setInnerValue] = useState(value)
+
+  useEffect(() => {
+    setInnerValue(value)
+  }, [value])
+
+  console.log('- 자식 Input 컴포넌트 rerendered (이슈 : 2번 리렌더되는걸 확인할 수 있다.)')
 
   return (
-    <>
-      <div>
-        <a href='https://vitejs.dev' target='_blank'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://react.dev' target='_blank'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className='card'>
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
-    </>
+    <input
+      value={innerValue}
+      onChange={(event) => {
+        const value = event.target.value
+        setInnerValue(value)
+        onChange(value)
+      }}
+    />
+  )
+}
+
+function App() {
+  const [outerValue, setOuterValue] = useState('')
+
+  console.log('- 부모 App 페이지 rerendered')
+
+  return (
+    <div style={{ display: 'flex', flexDirection: 'column' }}>
+      <Input value={outerValue} onChange={(value) => setOuterValue(value)} />
+      <input value={outerValue} onChange={(event) => setOuterValue(event.target.value)} />
+    </div>
   )
 }
 
