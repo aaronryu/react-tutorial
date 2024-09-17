bd3777125218eb83097b2dafa55a711cef7b2004
Aaron Ryu <fbcndah@gmail.com>
2024-09-18 00:59:22 +0900

    실습 22-2: 페이지 초기 로딩 시 Category API 및 옵션 선택 시 Product API 호출

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,48 @@ import { useEffect, useState } from 'react'
 import '@/App.css'
 
 function App() {
+  const [selected, setSelected] = useState('')
+  const [categories, setCategories] = useState([])
   const [products, setProducts] = useState([])
 
+  async function fetchCategories() {
+    const response = await fetch('https://fakestoreapi.com/products/categories')
+    const data = await response.json()
+    return data
+  }
+
+  async function fetchProducts(category) {
+    const response = await fetch(`https://fakestoreapi.com/products/category/${category}`)
+    const data = await response.json()
+    return data
+  }
+
   useEffect(() => {
-    fetch('https://fakestoreapi.com/products')
-      .then((response) => response.json())
-      .then((data) => setProducts(data))
-      .catch((error) => console.error('Error:', error))
+    async function fetches() {
+      const categories = await fetchCategories()
+      setCategories(categories)
+      setSelected(categories[0])
+    }
+    fetches()
   }, [])
+
+  useEffect(() => {
+    async function fetches() {
+      const products = await fetchProducts(selected)
+      setProducts(products)
+    }
+    fetches()
+  }, [selected])
 
   return (
     <div style={{ display: 'flex', flexDirection: 'column' }}>
+      <select defaultValue={selected} onChange={(e) => setSelected(e.target.value)}>
+        {categories.map((category) => (
+          <option key={category} value={category}>
+            {category}
+          </option>
+        ))}
+      </select>
       {products.map((product) => (
         <div key={product.id} style={{ textAlign: 'start' }}>
           <h3>{product.title}</h3>
