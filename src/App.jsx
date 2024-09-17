30316659045aa78b0d01e608751dd3d69ad4f6c9
Aaron Ryu <fbcndah@gmail.com>
2024-09-17 16:44:22 +0900

    실습 19-1: useContext 통해 간단한 <dialog> 모달 생성

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,40 @@
-import { useState } from 'react'
-import reactLogo from '@/assets/react.svg'
-import viteLogo from '/vite.svg'
+import { createContext, useContext, useState } from 'react'
 import '@/App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const ModalContext = createContext({ show: () => {}, close: () => {} })
+
+function ModalContextProvider({ children }) {
+  const [open, setOpen] = useState(false)
+
+  function show() {
+    setOpen(true)
+  }
+
+  function close() {
+    setOpen(false)
+  }
 
   return (
-    <>
-      <div>
-        <a href='https://vitejs.dev' target='_blank'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://react.dev' target='_blank'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className='card'>
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
-    </>
+    <ModalContext.Provider value={{ show, close }}>
+      {children}
+      <dialog open={open}>
+        <h3>Modal</h3>
+        <button onClick={(e) => close()}>닫기</button>
+      </dialog>
+    </ModalContext.Provider>
+  )
+}
+
+function ModalButton() {
+  const { show } = useContext(ModalContext)
+  return <button onClick={(e) => show()}>열기</button>
+}
+
+function App() {
+  return (
+    <ModalContextProvider>
+      <ModalButton />
+    </ModalContextProvider>
   )
 }
 
