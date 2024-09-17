f0c85e7c0c5f0a49101e9561107cedee268c8f51
Aaron Ryu <fbcndah@gmail.com>
2024-09-17 13:37:01 +0900

    실습 17-1: Props Drilling 및 State 사용처(View) 및 변경처(Controller) 분리

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,83 @@
 import { useState } from 'react'
-import reactLogo from '@/assets/react.svg'
-import viteLogo from '/vite.svg'
 import '@/App.css'
+
+function LC() {
+  console.log('- A.4. Fourth Component')
+  return (
+    <div className='component-box' style={{ padding: 10 }}>
+      Fourth Component
+    </div>
+  )
+}
+
+function TC({ count }) {
+  console.log('- A.3. Third Component')
+  return (
+    <div className='component-box' style={{ padding: 10 }}>
+      Third Component : {count}
+      <LC />
+    </div>
+  )
+}
+
+function SC({ count }) {
+  console.log('- A.2. Second Component')
+  return (
+    <div className='component-box' style={{ padding: 10 }}>
+      Second Component
+      <TC count={count} />
+    </div>
+  )
+}
+
+function FC({ count }) {
+  console.log('- A.1. First Component')
+  return (
+    <div className='component-box' style={{ padding: 10 }}>
+      First Component
+      <SC count={count} />
+    </div>
+  )
+}
+
+function ButtonComponent({ onClick }) {
+  console.log('- B. Button Component')
+  return (
+    <div className='component-box' style={{ padding: 10 }}>
+      Button Component
+      <div>
+        <button onClick={onClick}>증가</button>
+      </div>
+    </div>
+  )
+}
+
+function NonContextComponent({ count }) {
+  console.log('- C. Non-Context Component')
+  return (
+    <div className='component-box' style={{ padding: 10 }}>
+      Non-Context Component : {count}
+    </div>
+  )
+}
 
 function App() {
   const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href='https://vitejs.dev' target='_blank'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://react.dev' target='_blank'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className='card'>
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
-    </>
+    <div
+      className='section-box'
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        gap: 16,
+        padding: 10,
+      }}
+    >
+      <FC count={count} />
+      <ButtonComponent onClick={() => setCount((prev) => prev + 1)} />
+      <NonContextComponent count={count} />
+    </div>
   )
 }
 
