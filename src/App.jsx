10d1a0767c6870e51950927be82d52d69d92f9ad
Aaron Ryu <fbcndah@gmail.com>
2024-09-17 23:33:45 +0900

    실습 21-1: 무거운 계산이 필요한 값의 경우 useMemo 통해 계산 최소화

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,20 @@
-import { useState } from 'react'
-import reactLogo from '@/assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useMemo, useState } from 'react'
 import '@/App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [age, setAge] = useState(0)
+
+  /* 무거운 계산이 필요한 값의 경우 useMemo 통해 계산 횟수를 최소화할 수 있다. */
+  const valid = useMemo(() => {
+    console.log('calculated')
+    return age >= 19 ? true : false
+  }, [age >= 19])
 
   return (
     <>
-      <div>
-        <a href='https://vitejs.dev' target='_blank'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://react.dev' target='_blank'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className='card'>
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
+      <p>{age}</p>
+      <div>{valid ? <p>성년입니다</p> : <p style={{ color: 'red' }}>미성년입니다</p>}</div>
+      <button onClick={() => setAge(age + 1)}>증가</button>
     </>
   )
 }
