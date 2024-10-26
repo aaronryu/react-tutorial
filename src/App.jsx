d9b366e829549f535399a8a1eabf2d7af9fd091a
Aaron Ryu <fbcndah@gmail.com>
2024-10-26 23:31:58 +0900

    실습 25-2: Reducer + Action + (초기) State 설정을 위해 원하는 상태별(theme)로 Slice 생성

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,28 @@
 import '@/App.css'
+import { createSlice } from '@reduxjs/toolkit' // * RTK 에서 제공하는 간편 코드
+
+// 1. Slice 생성 (createSlice) = Reducer + Action + (초기) State 설정
+const themeSlice = createSlice({
+  name: 'theme',
+  // React 에서의 useReducer 사용할때도 2개의 파라미터가 필요했다 : useReducer((1) Reducer, (2) InitialState)
+  // (2) InitialState
+  initialState: { color: 'black', desc: '검정검정 테마' },
+  // (1) Reducer
+  reducers: {
+    // React 에서의 useReducer 처럼 새로운 상태를 return 하지않고, immer 사용했을때와 동일하게 변경할 상태만 골라서 변경하면되어, 간편하다.
+    change: (previousState) => {
+      if (previousState.color === 'black') {
+        // return { color: 'white', desc: '하얀하얀 테마' }
+        previousState.color = 'white'
+        previousState.desc = '하얀하얀 테마'
+      } else {
+        // return { color: 'black', desc: '검정검정 테마' }
+        previousState.color = 'black'
+        previousState.desc = '검정검정 테마'
+      }
+    },
+  },
+})
 
 function App() {
   return <></>
