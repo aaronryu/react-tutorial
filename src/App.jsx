88dcaf496235b95befd77ce863e3db130d454253
Aaron Ryu <fbcndah@gmail.com>
2024-10-27 16:41:24 +0900

    실습 29-1: Spring 간단한 User API 호출 및 <table> 출력 + Vite 내 CORS 프록시 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,83 @@
 import { useState } from 'react'
-import reactLogo from '@/assets/react.svg'
-import viteLogo from '/vite.svg'
-import '@/App.css'
+import './App.css'
+
+class CustomError extends Error {
+  constructor(detail) {
+    super('Custom Error 발생')
+    this.detail = detail
+  }
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [users, setUsers] = useState(undefined)
+  const [status, setStatus] = useState('idle')
+  // status: 'idle' | 'loading' | 'succeeded' | 'failed',
+
+  function fetchUsers() {
+    if (status !== 'idle') {
+      // 1. already fetched
+      console.log("already fetched! we don't need to fetch any api")
+      return
+    }
+    setStatus('loading')
+    // 2. fetch
+    fetch('/api/users')
+      .then((result) => {
+        console.log(result)
+        if (!result.ok) {
+          throw new CustomError({ status: result.status, statusText: result.statusText })
+        }
+        return result.json()
+      })
+      .then((fetchedUsers) => {
+        // 3. setState
+        setUsers(fetchedUsers)
+        setStatus('succeeded')
+      })
+      .catch((error) => {
+        console.log('에러 객체', error.detail)
+        console.log(error)
+        setStatus('failed')
+      })
+  }
 
   return (
     <>
-      <div>
-        <a href='https://vitejs.dev' target='_blank'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://react.dev' target='_blank'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className='card'>
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
+      <table>
+        <colgroup>
+          <col style={{ backgroundColor: '#89a795' }} width={80} />
+          <col style={{ backgroundColor: '#89a795' }} width={100} />
+          <col style={{ backgroundColor: '#89a795' }} width={120} />
+          <col style={{ backgroundColor: '#65796d' }} width={180} />
+        </colgroup>
+        <thead>
+          <tr style={{ backgroundColor: 'gray' }}>
+            <th>이름</th>
+            <th>직업</th>
+            <th>직무</th>
+            <th>회원가입일</th>
+          </tr>
+        </thead>
+        <tbody>
+          {status === 'loading' && (
+            <tr style={{ backgroundColor: '#c94c4c' }}>
+              <td colSpan={4}>Loading</td>
+            </tr>
+          )}
+          {status !== 'idle' &&
+            users.map((user) => (
+              <tr key={user.userId}>
+                <td>{user.username}</td>
+                <td>{user.job}</td>
+                <td>{user.specialty}</td>
+                <td>{user.createdAt}</td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
+      <button style={{ marginTop: 10 }} onClick={fetchUsers}>
+        조회하기
+      </button>
     </>
   )
 }
--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,15 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  server: {
+    proxy: {
+      '/api': 'http://127.0.0.1:8080',
+      // '/api': {
+      //   target: 'http://127.0.0.1:8080/api',
+      //   changeOrigin: true,
+      //   secure: false,
+      //   rewrite: (path) => path.replace(/^\/api/, ''),
+      // },
+    },
+  },
 })
