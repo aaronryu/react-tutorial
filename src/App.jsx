25666143dbd9b840166850247002c39a3518140e
Aaron Ryu <fbcndah@gmail.com>
2024-10-29 11:54:22 +0900

    실습 30-1: User API 로 조회한 유저 정보 표기를 위한 테이블 및 버튼

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,64 @@
 import { useState } from 'react'
-import reactLogo from '@/assets/react.svg'
-import viteLogo from '/vite.svg'
 import '@/App.css'
 
+function UserTable({ isLoading, isSuccess, users = [] }) {
+  return (
+    <>
+      <table>
+        <colgroup>
+          <col style={{ backgroundColor: '#89a795' }} width={80} />
+          <col style={{ backgroundColor: '#89a795' }} width={100} />
+          <col style={{ backgroundColor: '#89a795' }} width={120} />
+          <col style={{ backgroundColor: '#65796d' }} width={180} />
+        </colgroup>
+        <thead>
+          <tr style={{ backgroundColor: 'gray' }}>
+            <th>이름</th>
+            <th>직업</th>
+            <th>직무</th>
+            <th>회원가입일</th>
+          </tr>
+        </thead>
+        <tbody>
+          {isLoading && (
+            <tr style={{ backgroundColor: '#c94c4c' }}>
+              <td colSpan={4}>Loading</td>
+            </tr>
+          )}
+          {isSuccess &&
+            users.map((user) => (
+              <tr key={user.userId}>
+                <td>{user.username}</td>
+                <td>{user.job}</td>
+                <td>{user.specialty}</td>
+                <td>{user.createdAt}</td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
+    </>
+  )
+}
+
+function FetchButton({ refetch }) {
+  return (
+    <button style={{ marginTop: 10 }} onClick={(e) => refetch()}>
+      조회하기
+    </button>
+  )
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [isLoading, setIsLoading] = useState(false)
+  const [isSuccess, setIsSuccess] = useState(false)
+  const [users, setUsers] = useState()
+
+  function refetch() {}
 
   return (
     <>
-      <div>
-        <a href='https://vitejs.dev' target='_blank'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://react.dev' target='_blank'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className='card'>
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
+      <UserTable isLoading={isLoading} isSuccess={isSuccess} users={users} />
+      <FetchButton refetch={refetch} />
     </>
   )
 }
