8bf659479321e4e692da57f8ec23ce03f78942fd
Aaron Ryu <fbcndah@gmail.com>
2024-10-30 06:39:43 +0900

    실습 30-3: React Hook Form 통해 각각의 <input> 에 Validation 등록(register)

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import { forwardRef, useRef, useState } from 'react'
 import '@/App.css'
 import clsx from 'clsx'
+import { useForm } from 'react-hook-form'
 
 function UserTable({ isLoading, isSuccess, users = [] }) {
   return (
@@ -48,15 +49,23 @@ function FetchButton({ refetch }) {
 
 function ModalButton({ create }) {
   const dialog = useRef(null)
+  const {
+    handleSubmit,
+    register,
+    watch,
+    formState: { errors },
+  } = useForm()
 
   function onClickClose() {
     dialog.current.close()
   }
 
-  function onClickConfirm() {
+  function onClickConfirm(data) {
     dialog.current.close()
-    create()
+    create(data)
   }
+
+  console.log('현재 <input> 간의 에러 모음 : ', errors)
 
   return (
     <>
@@ -68,35 +77,46 @@ function ModalButton({ create }) {
         onClose={() => console.log('close 이벤트를 발생 = (1) esc 혹은 (2) 버튼')}
       >
         <h3 style={{ margin: 0 }}>유저 추가하기</h3>
-        <form className='form-wrapper' onSubmit={() => {}}>
-          <UsernameInput />
-          <EmailInput />
-          <AgeInput />
-          <JobInput />
-          <SpecialtyInput />
-          <button onClick={(e) => {}}>닫기</button>
-          <button onClick={(e) => {}}>저장하기</button>
+        <form className='form-wrapper' onSubmit={handleSubmit(onClickConfirm)}>
+          <UsernameInput register={register} error={errors?.username} />
+          <EmailInput register={register} error={errors?.email} />
+          <AgeInput register={register} error={errors?.age} />
+          <JobInput register={register} error={errors?.job} />
+          <SpecialtyInput register={register} error={errors?.specialty} />
+          <button type='button' onClick={onClickClose}>
+            닫기
+          </button>
+          <button type='submit'>저장하기</button>
         </form>
       </dialog>
     </>
   )
 }
 
-const Input = forwardRef(function ({ label, error }, ref) {
+const Input = forwardRef(function ({ label, error, ...others }, ref) {
   return (
     <div>
       <label className='input-label'>{label} : </label>
       <span className='input-area'>
-        <input ref={ref} className={clsx('input', error && 'error-border')} />
+        <input ref={ref} className={clsx('input', error && 'error-border')} {...others} />
       </span>
     </div>
   )
 })
 
-function UsernameInput({ error = undefined }) {
+function UsernameInput({ register, error = undefined }) {
   return (
     <div>
-      <Input label={'유저이름'} />
+      <Input
+        label={'유저이름'}
+        {...register('username', {
+          required: { value: true, message: '아이디를 입력해주세요' },
+          maxLength: {
+            value: 10,
+            message: '아이디는 10자 이상이 될 수 없습니다',
+          },
+        })}
+      />
       <div className={clsx(error && 'error-text')}>
         {error?.type === '_type_' && '_message_'}
         {error?.message}
@@ -105,10 +125,19 @@ function UsernameInput({ error = undefined }) {
   )
 }
 
-function EmailInput({ error = undefined }) {
+function EmailInput({ register, error = undefined }) {
   return (
     <div>
-      <Input label={'이메일'} />
+      <Input
+        label={'이메일'}
+        {...register('email', {
+          required: { value: true, message: '이메일을 입력해주세요' },
+          pattern: {
+            value: /^\S+@\S+$/i,
+            message: '이메일 형식이 올바르지 않습니다',
+          },
+        })}
+      />
       <div className={clsx(error && 'error-text')}>
         {error?.type === '_type_' && '_message_'}
         {error?.message}
@@ -117,10 +146,34 @@ function EmailInput({ error = undefined }) {
   )
 }
 
-function AgeInput({ error = undefined }) {
+function AgeInput({ register, error = undefined }) {
   return (
     <div>
-      <Input label={'나이'} type='number' />
+      <Input
+        label={'나이'}
+        type='number'
+        {...register('age', {
+          required: { value: true, message: '나이를 입력해주세요' },
+          min: 10,
+        })}
+      />
+      <div className={clsx(error && 'error-text')}>
+        {error?.type === 'min' && '나이는 10세 이상의 유저만 받을 수 있습니다'}
+        {error?.message}
+      </div>
+    </div>
+  )
+}
+
+function JobInput({ register, error = undefined }) {
+  return (
+    <div>
+      <Input
+        label={'직업'}
+        {...register('job', {
+          required: { value: true, message: '직업을 입력해주세요' },
+        })}
+      />
       <div className={clsx(error && 'error-text')}>
         {error?.type === '_type_' && '_message_'}
         {error?.message}
@@ -129,22 +182,15 @@ function AgeInput({ error = undefined }) {
   )
 }
 
-function JobInput({ error = undefined }) {
+function SpecialtyInput({ register, error = undefined }) {
   return (
     <div>
-      <Input label={'직업'} />
-      <div className={clsx(error && 'error-text')}>
-        {error?.type === '_type_' && '_message_'}
-        {error?.message}
-      </div>
-    </div>
-  )
-}
-
-function SpecialtyInput({ error = undefined }) {
-  return (
-    <div>
-      <Input label={'직무'} />
+      <Input
+        label={'직무'}
+        {...register('specialty', {
+          required: { value: true, message: '직무를 입력해주세요' },
+        })}
+      />
       <div className={clsx(error && 'error-text')}>
         {error?.type === '_type_' && '_message_'}
         {error?.message}
@@ -160,7 +206,9 @@ function App() {
 
   function refetch() {}
 
-  function create({ username, email, age, job, specialty }) {}
+  function create({ username, email, age, job, specialty }) {
+    console.log('회원가입 정보', { username, email, age, job, specialty })
+  }
 
   console.log('- 비제어 컴포넌트 : 매번 입력해도 rerender 미발생')
   return (
